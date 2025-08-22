dd6280cb9f83f7d12c45556aa8b18e9fd449f65a
Makhzum-Bin-Harun <76556113+makhzumbinharun@users.noreply.github.com>
2025-08-22 18:19:31 +0600

    Create Problem Q.c

---
+++ CONTEST 02/SOLUTIONS/Problem Q.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    int T;
+    scanf("%d", &T);
+
+    while (T--) {
+        long long N;
+        scanf("%lld", &N);
+
+        if (N == 0) {
+            printf("0\n");
+          // continue;
+        }
+        else {
+            while (N > 0) {
+                printf("%lld", N % 10);
+                N /= 10;
+                if (N > 0) printf(" ");
+            }
+            printf("\n");
+        }
+    }
+    return 0;
+}
